5f91c60ca840f98e56b476f12b381b37376ddb5e
Sameer Md <sameermd@Sameers-MacBook-Pro.local>
2023-01-19 01:42:19 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// // var name = "sameer"
+// // console.log(name)
+// // var name = "sana"
+// // console.log(name)
+// let cars = ["saab", "bmw", "audi"];
+// cars.push("toyota");
+// cars[0] = "ford";
+// console.log(cars)
